4d11eb62da12617a99944e47562f36e1ea0ae02e
leonlevrero <triplelevrero@gmail.com>
2022-08-12 12:00:49 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,4 +1,4 @@
-let url = `https://japceibal.github.io/emercado-api/cats_products/101.json`
+const url = "https://japceibal.github.io/emercado-api/cats_products/101.json"
 fetch(url)
  .then(response => response.json())
  .then(data => mostrarData(data))
@@ -6,9 +6,11 @@ fetch(url)
 
  const mostrarData = (data) => {
     console.log(data)
-    let body = ''
-    for (let i = 0; i<data.length; i++){
-        body += `<tr><td>${data[i].id}</td><td>${data[i].name}</td></tr>`
+    const body = '' // Aquí va el código HTML que se va a mostrar en el body
+    for (var i = 0; i<data.length; i++){
+        body += `<tr><td>${data[i].id}</td
+        <td>${data[i].name}</td></tr>`
+        
     }
-    document.getElementById('data').innerHTML = body
+    document.getElementById("data").innerText = body.innerText
  }
