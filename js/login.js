e2b9d670caa12067177e0f9ffff3d18daa14349c
leonlevrero <triplelevrero@gmail.com>
2022-08-14 01:19:44 -0300

    a

--- js/login.js
+++ js/login.js
@@ -1,16 +1,32 @@
 
+
 function succesCase() {
     const email = document.getElementById("floatingInput");
     const pass = document.getElementById("floatingPassword");
     const button = document.getElementById("reg");
+    let i= 0;
 
         button.addEventListener("click", () => {
             if(email.value != "" && pass.value != "") {
                 alert("Registro exitoso");
+                i = 1;
             }
             else{
                 alert("Error al registrar");
+                i = 0;
             }
+       
             })
- }  
+          
+}
  succesCase();
+ 
+
+function redirect(){
+    const boton = document.getElementById("redirect");
+    boton.addEventListener("click", () => {
+        location.href = "google.com";
+    } )
+}
+
+ 
